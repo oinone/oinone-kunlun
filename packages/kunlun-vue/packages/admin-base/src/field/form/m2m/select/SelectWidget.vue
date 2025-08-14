20ca685e6b61cfe4e0a794650850b653e141afe5
boLangShiLanYa <luojinliang@shushi.pro>
2025-08-14 11:37:39 +0800

    fix select支持可选模式

--- packages/kunlun-vue/packages/admin-base/src/field/form/m2m/select/SelectWidget.vue
+++ packages/kunlun-vue/packages/admin-base/src/field/form/m2m/select/SelectWidget.vue
@@ -5,7 +5,7 @@
         :max-tag-count="maxTagCount"
         :placeholder="placeholder"
         label-in-value
-        mode="multiple"
+        :mode="mode"
         :value="currentValue"
         :options="optionList"
       />
@@ -13,7 +13,7 @@
     <div v-else>
       <a-select
         ref="selectRef"
-        mode="multiple"
+        :mode="mode"
         class="oio-select"
         show-search
         label-in-value
@@ -48,7 +48,7 @@
   </div>
 </template>
 <script lang="ts">
-import { OioSpin, OioEmptyData } from '@oinone/kunlun-vue-ui-antd';
+import { OioEmptyData, OioSpin } from '@oinone/kunlun-vue-ui-antd';
 import { defineComponent, PropType, ref, watch } from 'vue';
 import { Select as ASelect } from 'ant-design-vue';
 import { RelationSelectProps, relationSelectSetup } from '../../../prop';
@@ -59,6 +59,10 @@ export default defineComponent({
     ...RelationSelectProps,
     value: {
       type: Array as PropType<Record<string, unknown>[]>
+    },
+    mode: {
+      type: String,
+      default: 'multiple'
     },
     generatorSelectOption: Function
   },
