bbe7a6a4aeac620ef48cc9aaaf437b14db059e82
fatelee <lijingtingd@shushi.pro>
2025-08-26 09:47:21 +0800

    fix:修复样式逻辑

--- packages/kunlun-vue/packages/admin-base/src/view/view/TableView.ts
+++ packages/kunlun-vue/packages/admin-base/src/view/view/TableView.ts
@@ -14,12 +14,27 @@ import { BaseListView, BaseView } from '../../basic';
  */
 @SPI.ClassFactory(BaseView.Token({ type: ViewType.Table }))
 export class TableView extends BaseListView {
+  protected $$beforeMount() {
+    super.$$beforeMount();
+    this.editRowCallChaining = new CallChaining<[string, ActiveRecords]>();
+  }
+
   @Widget.Reactive()
   @Widget.Provide()
   protected editRowCallChaining: CallChaining<[string, ActiveRecords]> | undefined;
 
-  protected $$beforeMount() {
-    super.$$beforeMount();
-    this.editRowCallChaining = new CallChaining<[string, ActiveRecords]>();
+
+  @Widget.Reactive()
+  @Widget.Provide()
+  protected fullScreen: boolean = this.defaultFullScreen;
+
+  @Widget.Reactive()
+  @Widget.Provide()
+  protected switchFullScreen() {
+    this.fullScreen = !this.fullScreen;
+  }
+
+  protected get defaultFullScreen() {
+    return this.getDsl().fullScreen || false;
   }
 }
