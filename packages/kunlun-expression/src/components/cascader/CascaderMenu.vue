b52587734f9378467d1efa1155ae56f38218ed99
boLangShiLanYa <luojinliang@shushi.pro>
2025-09-09 16:46:17 +0800

    feat 展示标签为name+field

--- packages/kunlun-expression/src/components/cascader/CascaderMenu.vue
+++ packages/kunlun-expression/src/components/cascader/CascaderMenu.vue
@@ -6,8 +6,8 @@
       </div>
       <div class="expression-designer-cascader-menu-inner-content">
         <div
-          v-for="option in realOptions"
-          :key="option.value"
+          v-for="(option,index) in realOptions"
+          :key="index"
           class="ant-cascader-menu-item ant-cascader-menu-item-expand"
           :class="{
             'ant-cascader-menu-item-group': option.optType === 'group',
@@ -33,7 +33,8 @@
               :class="getTtypeIcon(option)"
               class="iconfont menu-icon"
             />
-            {{ option.label }}
+            <span>{{ option.label }}</span>
+            <span class="source-code">{{ option.field }}</span>
           </div>
           <div class="ant-cascader-menu-item-expand-icon" v-if="showArrowRight(option)" @click.stop="loadData(option)">
             <span role="img" aria-label="right" class="anticon anticon-right">
