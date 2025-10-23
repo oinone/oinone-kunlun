2b7c05bd6941cee15379765eede11a49d8ce2e73
fatelee <lijingtingd@shushi.pro>
2025-10-23 18:42:48 +0800

    fix: fix reference field model error

--- packages/kunlun-expression/src/components/control/condition/ConditionInputForm.vue
+++ packages/kunlun-expression/src/components/control/condition/ConditionInputForm.vue
@@ -280,7 +280,7 @@
   </div>
 </template>
 <script lang="ts">
-import { deepClone, IModelField, isComplexTtype } from '@oinone/kunlun-meta';
+import { deepClone, IModelField } from '@oinone/kunlun-meta';
 import { OioTextarea } from '@oinone/kunlun-vue-ui-antd';
 import {
   Checkbox as ACheckbox,
@@ -739,6 +739,7 @@ export default defineComponent({
     function createExpressionItemLeftJoinField(expressionItem: IExpressionItem): IModelField {
       if (expressionItem.valueList && expressionItem.valueList.length) {
         const field = expressionItem.valueList[0];
+        console.log('🚀 ~ createExpressionItemLeftJoinField ~ field:', field);
         if (!field.value) {
           return undefined as any as IModelField;
         }
@@ -753,7 +754,7 @@ export default defineComponent({
           }
         }
 
-        const model = (isComplexTtype(field.ttype!) ? field.references : field.model) || modelModel;
+        const model = field.references || field.model || modelModel;
 
         return {
           name: fieldName,
