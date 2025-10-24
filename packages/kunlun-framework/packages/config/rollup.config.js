853580e4991696bc7184fbfcc392fb4445e73098
fatelee <lijingtingd@shushi.pro>
2025-10-24 10:42:58 +0800

    feat: 完善打包配置

---
+++ packages/kunlun-framework/packages/config/rollup.config.js
@@ -0,0 +1,4 @@
+import pkg from './package.json';
+import rollupConfig from '../../scripts/build.config.js';
+
+export default rollupConfig(pkg.name, ['@oinone/kunlun-spi', '@oinone/kunlun-shared']);
