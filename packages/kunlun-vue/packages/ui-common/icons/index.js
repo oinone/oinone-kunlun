cbab9f3714c79d214de57b8caa9f00fbd31ebd50
zbh <zbh@shushi.pro>
2025-11-27 11:52:24 +0800

    feat: update icon

--- packages/kunlun-vue/packages/ui-common/icons/index.js
+++ packages/kunlun-vue/packages/ui-common/icons/index.js
@@ -1,5 +1,8 @@
-import './iconfont/iconfont.css';
-import './iconfont/iconfont.js';
+import './iconfont-deprecated/iconfont.css';
+import './iconfont-deprecated/iconfont.js';
 
 import './oinone-iconfont/iconfont.css';
 import './oinone-iconfont/iconfont.js';
+
+import './iconfont/iconfont.css';
+import './iconfont/iconfont.js';
