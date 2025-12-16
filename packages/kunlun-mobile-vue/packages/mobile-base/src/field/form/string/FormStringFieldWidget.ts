9acca94131f4eb94aa6227af80684cae29d5a905
zbh <zbh@shushi.pro>
2025-12-16 18:48:43 +0800

    fix: validateLength maxLength and minLength

--- packages/kunlun-mobile-vue/packages/mobile-base/src/field/form/string/FormStringFieldWidget.ts
+++ packages/kunlun-mobile-vue/packages/mobile-base/src/field/form/string/FormStringFieldWidget.ts
@@ -129,10 +129,10 @@ export class FormStringFieldWidget extends FormInputAbstractFieldWidget<string, 
   protected validateLength(realValue: string | undefined): ValidatorInfo {
     const length = !isNil(realValue) ? realValue.length : 0;
     const { minLength, maxLength } = this;
-    if (!isEmptyValue(realValue) && !isNil(maxLength) && length > maxLength) {
+    if (!isEmptyValue(realValue) && !isNil(maxLength) && maxLength >= 0 && length > maxLength) {
       return this.validatorError(`${translateValueByKey('最大长度为')} ${maxLength}`);
     }
-    if (!isEmptyValue(realValue) && !isNil(minLength) && length < minLength) {
+    if (!isEmptyValue(realValue) && !isNil(minLength) && minLength >= 0 && length < minLength) {
       return this.validatorError(`${translateValueByKey('最小长度为')} ${minLength}`);
     }
     return this.validatorSuccess();
