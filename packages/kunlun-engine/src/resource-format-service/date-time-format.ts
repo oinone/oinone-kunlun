40d99c80e487ed80fbcfa41ce0863de09bfe49f7
boLangShiLanYa <luojinliang@shushi.pro>
2025-12-22 11:47:12 +0800

    fix:queryResourceDateTimeFormat兼容毫秒字段缺失

--- packages/kunlun-engine/src/resource-format-service/date-time-format.ts
+++ packages/kunlun-engine/src/resource-format-service/date-time-format.ts
@@ -47,6 +47,39 @@ async function queryResourceDateTimeFormat0() {
 
   const code = await CurrentLanguage.getCode();
 
+  const timeModel = await ModelCache.get('resource.ResourceTimeFormat');
+
+  const sssTimeformatExisting = timeModel?.modelFields.some((m) => m.name === 'apColonNormalSss');
+
+  let resourceTimeFormat = `
+  resourceTimeFormat {
+        apColonNormal
+        colonNormal
+        apColonShort
+        colonShort
+        apColonNormalMap
+        colonNormalMap
+        apColonShortMap
+        colonShortMap
+      }`;
+  if (sssTimeformatExisting) {
+    resourceTimeFormat = `
+    resourceTimeFormat {
+        apColonNormal
+        apColonNormalSss
+        colonNormal
+        colonNormalSss
+        apColonShort
+        colonShort
+        apColonNormalMap
+        apColonNormalSssMap
+        colonNormalMap
+        colonNormalSssMap
+        apColonShortMap
+        colonShortMap
+      }`;
+  }
+
   const body = `{
   resourceLangQuery {
     queryOne(query: {code: "${code}"}) {
@@ -65,20 +98,7 @@ async function queryResourceDateTimeFormat0() {
         slashYearMonthMap
         chineseYearMonthMap
       }
-      resourceTimeFormat {
-        apColonNormal
-        apColonNormalSss
-        colonNormal
-        colonNormalSss
-        apColonShort
-        colonShort
-        apColonNormalMap
-        apColonNormalSssMap
-        colonNormalMap
-        colonNormalSssMap
-        apColonShortMap
-        colonShortMap
-      }
+      ${resourceTimeFormat}
     }
   }
 }
