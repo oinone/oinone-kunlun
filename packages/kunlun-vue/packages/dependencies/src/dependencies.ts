3fbece261b7b8ddfaf9bc4ccef5cdd51446eddc1
zbh <zbh@shushi.pro>
2025-12-29 15:59:08 +0800

    fix: build

--- packages/kunlun-vue/packages/dependencies/src/dependencies.ts
+++ packages/kunlun-vue/packages/dependencies/src/dependencies.ts
@@ -1,5 +1,5 @@
 import '@oinone/kunlun-vue-ui-common/dist/oinone-kunlun-vue-ui-common.css';
-import '@oinone/kunlun-vue-ui/dist/oinone-kunlun-vue-ui.css';
+// import '@oinone/kunlun-vue-ui/dist/oinone-kunlun-vue-ui.css';
 // import '@oinone/kunlun-vue-admin-layout/dist/oinone-kunlun-vue-admin-layout.css';
 // import '@oinone/kunlun-vue-admin-base/dist/oinone-kunlun-vue-admin-base.css';
 import '@oinone/kunlun-vue-expression/dist/oinone-kunlun-vue-expression.css';
