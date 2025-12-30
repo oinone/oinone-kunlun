905f749621cf1c49a5e798793308624bc86f8c81
zbh <zbh@shushi.pro>
2025-12-30 10:17:18 +0800

    fix: build

--- packages/kunlun-compile/src/quick-builder.ts
+++ packages/kunlun-compile/src/quick-builder.ts
@@ -38,12 +38,11 @@ export const rollupConfig = ({
   copyTypeFiles,
   debug
 }: QuickBuilderOptions) => {
+  const defaultExternal: (string | RegExp)[] = [];
+  pushExternal(defaultExternal, Object.keys(pkg.dependencies || {}));
+  pushExternal(defaultExternal, Object.keys(pkg.devDependencies || {}));
   const finalExternal = [
-    ...new Set([
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.devDependencies || {}),
-      ...(includeExternal || [])
-    ]).difference(new Set([...(excludeExternal || [])]))
+    ...new Set([...defaultExternal, ...(includeExternal || [])]).difference(new Set([...(excludeExternal || [])]))
   ];
   const builder = CompileConfigBuilder.config(debug)
     .prefix(pkg.name, prefix)
@@ -79,3 +78,15 @@ export const rollupConfig = ({
   }
   return builder.build();
 };
+
+function escapeRegExp(str) {
+  // 正则特殊字符：^ $ \ . * + ? | ( ) [ ] { } ,
+  return str.replace(/[\\^$.*+?|()[\]{}]/g, '\\$&');
+}
+
+function pushExternal(array: (string | RegExp)[], external: string[]) {
+  for (const value of external) {
+    array.push(value);
+    array.push(new RegExp(`^${escapeRegExp(value)}`));
+  }
+}
