534bd0a7727897c3a926e8d91b18d0265b2d0506
zbh <zbh@shushi.pro>
2025-12-30 19:57:26 +0800

    fix: remove includeExternal

--- packages/kunlun-framework/packages/shared/rollup.config.js
+++ packages/kunlun-framework/packages/shared/rollup.config.js
@@ -4,16 +4,5 @@ import { rollupConfig } from '@oinone/kunlun-compile';
 export default rollupConfig({
   pkg,
   hasVue: false,
-  hasSCSS: false,
-  includeExternal: [
-    '@rsql/builder',
-    '@rsql/emitter',
-    '@rsql/parser',
-    'bignumber.js',
-    'lodash',
-    'lodash-es',
-    'dayjs',
-    'crypto-js',
-    'moment'
-  ]
+  hasSCSS: false
 });
