e6ec8cc7371a0582f98c57e544414d48eba03f71
zbh <zbh@shushi.pro>
2026-01-01 20:30:13 +0800

    fix: build

--- packages/kunlun-compile/eslint/common.cjs
+++ packages/kunlun-compile/eslint/common.cjs
@@ -9,6 +9,7 @@ const common = {
     'vue/no-unused-components': 'warn',
     'vue/multi-word-component-names': 'warn',
     'vue/block-lang': 'warn',
+    'vue/no-unused-vars': 'warn',
 
     '@typescript-eslint/no-unsafe-function-type': 'off',
     '@typescript-eslint/no-wrapper-object-types': 'off',
