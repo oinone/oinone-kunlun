e84968c289c8742e45f77e1e7d26d52628feab82
zbh <zbh@shushi.pro>
2026-01-01 19:23:40 +0800

    fix: huskyrc

--- packages/kunlun-compile/eslint/common.cjs
+++ packages/kunlun-compile/eslint/common.cjs
@@ -6,6 +6,9 @@ const common = {
     '@typescript-eslint/no-unused-expressions': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
     'prefer-rest-params': 'warn',
+    'vue/no-unused-components': 'warn',
+    'vue/multi-word-component-names': 'warn',
+    'vue/block-lang': 'warn',
 
     '@typescript-eslint/no-unsafe-function-type': 'off',
     '@typescript-eslint/no-wrapper-object-types': 'off',
