2ecde51078ebc65ee05b9e8e2af08c7adf1000cb
zbh <zbh@shushi.pro>
2026-01-13 18:24:12 +0800

    fix: inline actionBar

--- packages/kunlun-vue/packages/admin-base/src/action/component/action-bar/DefaultActionBar.vue
+++ packages/kunlun-vue/packages/admin-base/src/action/component/action-bar/DefaultActionBar.vue
@@ -78,7 +78,7 @@ function createMoreAction(
       OioDropdown,
       {
         overlayClassName: 'default-dropdown-overlay',
-        trigger: [OioDropdownTrigger.hover]
+        trigger: options.moreActionTriggers
       },
       {
         default: () => [triggerVNode],
@@ -254,7 +254,7 @@ export default defineComponent({
         buttonType: this.buttonType,
         operatorColumnDirection: operatorColumnDirection as OperationColumnDirection,
         allMounted: this.allMounted,
-        moreActionTriggers: this.moreActionTriggers!
+        moreActionTriggers: this.moreActionTriggers || [OioDropdownTrigger.hover]
       });
       if (Array.isArray(renderResult)) {
         moreActionVNodes = renderResult;
