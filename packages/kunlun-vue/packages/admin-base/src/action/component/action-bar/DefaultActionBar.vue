46c31a0cff9bdb9756a2106b1a076bafdd4d2797
zbh <zbh@shushi.pro>
2026-01-22 12:47:45 +0800

    fix: build

--- packages/kunlun-vue/packages/admin-base/src/action/component/action-bar/DefaultActionBar.vue
+++ packages/kunlun-vue/packages/admin-base/src/action/component/action-bar/DefaultActionBar.vue
@@ -255,7 +255,7 @@ export default defineComponent({
       'left',
       'right'
     ]);
-    const collectionActions = new CollectionActions(this.showActionNames, 2);
+    const collectionActions = new CollectionActions(this.showActionNames, this.activeCount);
     collectionActions.do(defaultSlot());
     const { hasMore, showActionFlags, moreActions, moreActionFlags, otherVNodes } = collectionActions;
     let { showActions } = collectionActions;
