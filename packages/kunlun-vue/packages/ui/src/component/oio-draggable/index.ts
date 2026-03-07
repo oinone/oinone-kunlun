72cc10b0e0b063fe4a0a23717b523249083750c5
zbh <zbh@shushi.pro>
2026-03-07 18:00:46 +0800

    feat: add override vuedraggable

--- packages/kunlun-vue/packages/ui/src/component/oio-draggable/index.ts
+++ packages/kunlun-vue/packages/ui/src/component/oio-draggable/index.ts
@@ -1,3 +1,4 @@
 export * from './event';
 export * from './props';
 export { default as OioDraggable } from './oio-draggable.vue';
+export { default as VueDraggable } from './override/vuedraggable.js';
