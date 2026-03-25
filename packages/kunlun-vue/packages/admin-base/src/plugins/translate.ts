2ec4bedb3acb376ac2a6cefb4512fae917978255
zbh <zbh@shushi.pro>
2026-03-25 12:37:05 +0800

    feat: translate

--- packages/kunlun-vue/packages/admin-base/src/plugins/translate.ts
+++ packages/kunlun-vue/packages/admin-base/src/plugins/translate.ts
@@ -1,11 +1,10 @@
 import { translateValueByKey } from '@oinone/kunlun-engine';
-import type { GenericType, StandardString } from '@oinone/kunlun-shared';
 import type { Plugin } from 'vue';
 
 export const $translate: Plugin = {
   install: (app) => {
-    app.config.globalProperties.$translate = <T extends StandardString = StandardString>(text: T): T => {
-      return translateValueByKey(text as unknown as GenericType<T>) as unknown as T;
+    app.config.globalProperties.$translate = (text: string, context?: Record<string, unknown>): string => {
+      return translateValueByKey(text, context);
     };
   }
 };
